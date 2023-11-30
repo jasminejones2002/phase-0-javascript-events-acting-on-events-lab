40457dd4898188824a41c0d25dbdb1e18fdcdfbb
Jasmine <jazziejones02@yahoo.com>
2023-11-29 23:02:08 -0600

    acting on events lab completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+function moveDodgerRight() {
+    let newPosition = dodger.style.left
+    newPosition = parseInt(newPosition)
+
+    if (newPosition < 360){
+        dodger.style.left = `${newPosition + 1}px`;
+    }
+}
